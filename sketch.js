8b7310e332dad115b2b6d62e5fd57b31a173c043
utkrishtmishra <75085169+utkrishtmishra@users.noreply.github.com>
2022-10-23 16:51:32 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -55,7 +55,7 @@ function draw() {
     bob5.display();
 }
 
-function KeyPressed() {
+function keyPressed() {
     if (keyCode === UP_ARROW) {
         Matter.Body.applyForce(bob1.body, bob1.body.position, { x: -50, y: -45 });
     }
